f1d0836276b06210989e092c91058d06737da950
Amelie Freeman <amelie@amelierosalyn.com>
2021-12-01 14:23:49 +0000

    Refactor

--- 1.rb
+++ 1.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/env ruby
 FILE = File.readlines('input/1.txt').map(&:to_i)
 
+def number_increased?(number, counter, source = FILE)
+  false if counter < 1
+  number > source[counter-1].to_i
+end
+
 def part_1
   increases = 0
 
   FILE.each_with_index do |n, i|
-    next if i < 1
-    increases += 1 if n > FILE[i-1].to_i
+    increases += 1 if number_increased?(n, i)
   end
 
   increases
@@ -17,7 +21,11 @@ def part_2
   increases = 0
 
   FILE.each_with_index do |n, i|
-    three_numbers = [n, FILE[i+1].to_i, FILE[i+2].to_i] rescue []
+    three_numbers = [n]
+    
+    1.upto(2) do |j|
+      three_numbers << FILE[i+j].to_i rescue nil
+    end
 
     break if three_numbers.empty?
 
@@ -25,8 +33,7 @@ def part_2
   end
 
   sums.each_with_index do |n, i|
-    next if i < 1
-    increases += 1 if n > sums[i-1].to_i
+    increases += 1 if number_increased?(n, i, sums)
   end
 
   increases
