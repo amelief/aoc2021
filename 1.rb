b2bee67c9c914dd52c9357433acb76e4215f3671
Amelie Freeman <amelie@amelierosalyn.com>
2021-12-01 14:26:05 +0000

    Refactor

--- 1.rb
+++ 1.rb
@@ -3,7 +3,7 @@ FILE = File.readlines('input/1.txt').map(&:to_i)
 
 def number_increased?(number, counter, source = FILE)
   false if counter < 1
-  number > source[counter-1].to_i
+  number > source[counter - 1].to_i
 end
 
 def part_1
@@ -24,7 +24,7 @@ def part_2
     three_numbers = [n]
     
     1.upto(2) do |j|
-      three_numbers << FILE[i+j].to_i rescue nil
+      three_numbers << FILE[i + j].to_i rescue nil
     end
 
     break if three_numbers.empty?
